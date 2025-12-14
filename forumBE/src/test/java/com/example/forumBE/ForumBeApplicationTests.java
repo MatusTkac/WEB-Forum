b3518911ab11e6a3eba5a31c68b948fe9f7fc7a9
Filip Kastella <kastellafilip@gmail.com>
2025-12-14 12:07:37 +0100

    Add initial project structure with Maven and Spring Boot setup

---
+++ forumBE/src/test/java/com/example/forumBE/ForumBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forumBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
