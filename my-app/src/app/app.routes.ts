d1747813e11e5641a900c3402e1525fb27a87edb
“[matus <tkac@provys.com>
2025-12-14 12:41:58 +0100

    fixed routes

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Users } from '../app/pages/users/users';
+import { Login } from '../app/pages/login/login';
+import { NotFound } from '../app/pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'users', component: Users},
+  {path: 'login', component: Login},
+  {path: '', pathMatch: 'full', redirectTo: '/login'},
+  {path: '**', component: NotFound}
+];
